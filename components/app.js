f951e78c0e7cfee707620718745d2a6d9251b91d
Axhutoxh <ashutosh.chandra.520@gmail.com>
2020-12-10 22:06:09 +0530

    Vue_Components_fundamentals

--- components/app.js
+++ components/app.js
@@ -1,23 +1,53 @@
-Vue.component('task-picker',{
-    template:'#task-picker',
-    data(){
-        return{
-            tasks:['Go to Gym','Prepare Breakfast','Go to work','rest'],
-        }
-    }
-})
-
-Vue.component('task',{
+let Tasks ={
     template:'#add_task',
     props:{
         name:{
             type:String,
             required:true
+        },
+        selectedTask:{
+            type:String
+        }
+    },
+    computed:{
+        isSelected(){
+            return this.name === this.selectedTask
+        }
+    },
+    methods:{
+        select(){
+            this.$emit('select',this.name)
+
         }
     }
-})
+}
+
+
+let Taskpicker ={
+    template:'#task-picker',
+    components:{
+        task:Tasks
+    },
+    data(){
+        return{
+            tasks:['Go to Gym','Prepare Breakfast','Go to work','rest'],
+
+            selectedTask:null
+        }
+    },
+    methods:{
+        selectTask(task){
+            this.selectedTask=task
+        }
+    }
+}
+
+
 
 new Vue({
     el:'#app',
+    components:{
+        'task-picker':Taskpicker
+    }
 
     })
